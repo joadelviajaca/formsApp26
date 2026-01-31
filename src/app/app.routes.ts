b6398fb7b07cd4b8ae3c0d8eff23b45dd96912eb
joadelvia <joadelvia@gmail.com>
2026-01-31 08:56:29 +0100

    Rutas reactive creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { Home } from './home/home';
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'template', loadChildren: () => import('./template/routes').then( m => m.TEMPLATE_ROUTES ) },
+    { path: 'reactive', loadChildren: () => import('./reactive/routes').then( m => m.REACTIVE_ROUTES ) },
 ];
