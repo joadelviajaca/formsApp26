63734671eb689c61a5bbd6569ad47a26742181b1
joadelvia <joadelvia@gmail.com>
2026-02-01 09:50:32 +0100

    Template dinámicos terminado

--- src/app/template/dinamicos-component/dinamicos-component.ts
+++ src/app/template/dinamicos-component/dinamicos-component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Favorito, Persona } from '../../interfaces';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dinamicos-component',
-  imports: [],
+  imports: [JsonPipe,FormsModule],
   templateUrl: './dinamicos-component.html',
 })
 export class DinamicosComponent {
+
+  @ViewChild('myForm') myForm!: NgForm;
+
+  name: string = "";
+  persona : Persona = {
+    nombre: "Joaquín", 
+    favoritos: [{id: 1, nombre: "Fornite"}, {id: 2, nombre: "Super Mario"}]
+  }
+
+  id: number = 3;
+
+  notValid(field: string): boolean {
+    return this.myForm?.controls[field]?.invalid && this.myForm?.controls[field].touched
+  }
+
+  agregarFavorito(){
+    const nuevoFavorito: Favorito = {
+      id: this.id,
+      nombre: this.name
+    }
+    this.persona.favoritos.push(nuevoFavorito);
+    this.id++;
+    this.name = '';
+  }
+
+  borrarFavorito(id: number){
+    this.persona.favoritos = this.persona.favoritos.filter(favorito => favorito.id !== id)
+  }
+
+  submit(){
+    this.myForm.control.markAllAsTouched()
+  }
 
 }
