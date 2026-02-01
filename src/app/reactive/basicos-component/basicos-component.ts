c225adffc704e690b5cf11739815ff23597e3e0e
joadelvia <joadelvia@gmail.com>
2026-02-01 08:36:43 +0100

    Template básicos terminado

--- src/app/reactive/basicos-component/basicos-component.ts
+++ src/app/reactive/basicos-component/basicos-component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './basicos-component.html',
 })
 export class BasicosComponent {
-
 }
--- src/app/template/basicos-component/basicos-component.ts
+++ src/app/template/basicos-component/basicos-component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos-component',
-  imports: [],
+  imports: [FormsModule,JsonPipe],
   templateUrl: './basicos-component.html',
 })
 export class BasicosComponent {
+  @ViewChild('myForm') myForm!: NgForm;
 
+  notValidField(fieldName: string): boolean {
+    return this.myForm?.controls[fieldName]?.invalid && this.myForm?.controls[fieldName]?.touched;
+  }
+
+  notValidPrice() {
+    if (this.myForm?.controls['precio'].value < 0) { // Validación personalizada
+        this.myForm.controls['precio'].setErrors({ precio: true }); // Añadimos el error
+        return false;
+    } else {
+        this.myForm.controls['precio'].setErrors(null); // Eliminamos el error
+        return true;
+    }
+}
+
+  save( form: NgForm) {
+    console.log(form);
+    // Aquí iría la lógica para guardar el formulario
+    // Reseteamos el formulario
+    this.myForm.resetForm({
+      producto: 'RTX 4090',
+      precio: 1600,
+      existencias: 10
+    });
+    }
 }
